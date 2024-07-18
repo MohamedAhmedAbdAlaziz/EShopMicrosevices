5d81346816dcbf8297b85644385eb1afc3f3219d
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-07-19 00:16:35 +0300

    Add validation Behavour

--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -9,11 +9,34 @@ namespace Catalog.API.Products.CreateProduct
       decimal Price) : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
-    public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    public class CreateProductCommandValidator:AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Requesrd");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Requesrd");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is Requesrd");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price is Requesrd");
+
+        }
+     
+    }
+    public class CreateProductCommandHandler(IDocumentSession session
+       // ,IValidator<CreateProductCommand> validator
+       ,ILogger<CreateProductCommandHandler> Logger
+        ) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command,
             CancellationToken cancellationToken)
         {
+            //var result = await validator.ValidateAsync(command, cancellationToken);
+            //    var errors= result.Errors.Select(x => x.ErrorMessage).ToList();
+            //if (errors.Any())
+            //{
+            //    throw new ValidationException(errors.FirstOrDefault());
+            //}
+            Logger.LogInformation("CreateProductCommandHandler.Hanlder called with {@command}", command);
             var Product = new Product
             {
                 Name = command.Name,
--- src/Catalog.API/Program.cs
+++ src/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using BuildingBlocks.Behaviors;
 using Carter;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,10 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);   
 builder.Services.AddMarten(option =>
 {
     option.Connection(builder.Configuration.GetConnectionString("Database")!);
